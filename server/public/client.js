3bf0063f4d7107b939036f4a87ab75d2f4d21d67
Tyler Burns <Tburnsie10@gmail.com>
2023-05-17 16:51:12 -0400

    adding bootstrap

--- server/public/client.js
+++ server/public/client.js
@@ -7,11 +7,27 @@ function formSubmit(event) {
     let num2 = inputs[1].value;
     let op = document.querySelector('select').value
     console.log(eval(num1 + op + num2))
+
     
+
+    fetch("/calc", {method: 'POST',body: JSON.stringify({"op":num1 + op + num2}), headers: {Accept: 'application.json','Content-Type': 'application/json'}})
+    .then(response => {
+        console.log(response.status)
+    })
+    
+    fetch("/calc")
+    .then(response => response.json())
+    .then(history => {
+        document.querySelector("#result").innerHTML = `<h3>${history[0]}</h3>`;
+        console.log(history.slice(1))
+        let his = document.querySelector("#history")
+        his.innerHTML = '';
+        for(let item of history.slice(1)) {
+            his.innerHTML += `<a class="list-group-item list-group-item-action list-group-item-dark">${item}</a>`
+            
+        }
+    })
 }
-
-
-
 
 
 function reset(event) {
--- server/server.js
+++ server/server.js
@@ -1,21 +1,26 @@
 const express = require('express' );
- const app = express();
- const PORT = 8000;
+const app = express();
+const PORT = 8000;
 
- let history = [];
- //save all our past functions
-
-
- app.use(express.json());
+let history = [];
+//save all our past functions
 
 
+app.use(express.json());
+app.use(express.static('server/public'));
+
+app.post("/calc", (req, res) => {
+   let op = req.body.op
+   history.unshift(eval(op))
+   console.log(op)
+   res.sendStatus(201)
+})
+
+app.get("/calc", (req, res) => {
+   res.status(200).send(history);
+} )
 
 
- app.use(express.static('server/public'));
-
-
-
-
- app.listen(PORT, () => {
-    console.log('server running on: ', PORT);
- });
+app.listen(PORT, () => {
+   console.log('server running on: ', PORT);
+});
